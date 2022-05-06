bafb259010d97e4e9b14f48fb9b511d1486f372f
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-05-05 22:26:16 -0300

    listando todas as perguntas

---
+++ src/main/java/com/npteam/apinotivis/controller/PerguntaController.java
@@ -0,0 +1,32 @@
+package com.npteam.apinotivis.controller;
+
+
+import com.npteam.apinotivis.dao.PerguntaDAO;
+import com.npteam.apinotivis.model.Aluno;
+import com.npteam.apinotivis.model.Pergunta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PerguntaController {
+
+  @Autowired
+  private PerguntaDAO perguntaDAO;
+
+  @GetMapping("/perguntas/listar/todas")
+  public ResponseEntity<List<Pergunta>> listarTodasPerguntas() {
+    ResponseEntity<List<Pergunta>> response = ResponseEntity.ok(perguntaDAO.findAll());
+    if (response != null) {
+      return response;
+    }
+    return ResponseEntity.notFound().build();
+
+    //return ResponseEntity.ok(perguntaDAO.findAll());
+  }
+
+
+}
--- src/main/java/com/npteam/apinotivis/dao/PerguntaDAO.java
+++ src/main/java/com/npteam/apinotivis/dao/PerguntaDAO.java
@@ -2,6 +2,8 @@ package com.npteam.apinotivis.dao;
 
 import com.npteam.apinotivis.model.Pergunta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PerguntaDAO extends JpaRepository<Pergunta, Integer> {
 }
